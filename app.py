58473a40fcf5c06e1ef3d67f8fc93ab366d609ce
akhila <karanam.akhila2003@gmail.com>
2023-04-23 20:48:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,Blueprint , render_template , url_for , redirect , request , session
+import bcrypt
+import random
+import smtplib
+import pymongo
+# from flask_pymongo import PyMongo
+
+
+def create_app():
+    app=Flask(__name__)
+    # app.config.from_object('config')
+    # app.config['MONGO_URI']="mongodb+srv://Karanam_Akhila:An12$shi@cluster0.h1vwyen.mongodb.net/?retryWrites=true&w=majority"
+    # mongo=PyMongo(app)
+    from views import views
+    app.register_blueprint(views,url_prefix="/")
+
+    from auth import auth
+    app.register_blueprint(auth,url_prefix="/")
+
+
+    return app
+
+if __name__=="__main__":
+    app=create_app()
+    app.run(debug=True)
+
+    
